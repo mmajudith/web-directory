9975bfb09710a52fc8e18cb821540e6a6f342b2f
mmajudith <mmajudith2000@gmail.com>
2023-06-09 12:54:18 -0700

    fix
    Fix module issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,4 @@
 import { Nunito } from 'next/font/google';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
@@ -22,17 +20,6 @@ export default function RootLayout({ children }) {
 			<body
 				className={`${nunito.className} w-full text-base font-normal bg-white not-italic`}
 			>
-				{/* <ToastContainer
-					position="top-center"
-					autoClose={5000}
-					hideProgressBar={false}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-				/> */}
 				<Header />
 				{children}
 				<Footer />
