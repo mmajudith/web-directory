3d0d4d6171bb6c725f2e340b1da3d9107a73bf13
mmajudith <mmajudith2000@gmail.com>
2024-02-19 15:46:10 -0800

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
-import Header from '@/components/header/Header';
-import Footer from '@/components/footer/Footer';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
 
 export const metadata = {
 	title: 'Dutiful',
